a1e6a8c9fb51efd6537a8cb4a726df73066af8b8
jbandura <j.bandura@borderstudio.eu>
2015-12-11 09:18:54 +0100

    add safety check for some validators value

--- addon/validators/between-validator.js
+++ addon/validators/between-validator.js
@@ -4,7 +4,7 @@ export default Ember.Object.extend({
   validate(value, validator) {
     let minLength = parseInt(validator.get('arguments')[0]);
     let maxLength = parseInt(validator.get('arguments')[1]);
-    let isValid = value.length >= minLength && value.length <= maxLength;
+    let isValid = value && value.length >= minLength && value.length <= maxLength;
     if (!isValid) {
       return 'message', 'must be between';
     }
--- addon/validators/min-validator.js
+++ addon/validators/min-validator.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   validate(value, validator) {
     let length = parseInt(validator.get('arguments')[0]);
-    let isValid = value.length >= length;
+    let isValid = value && value.length >= length;
     if (!isValid) {
       return 'too short';
     }
