d50e072f991830b9421cdbe169b47cec0d7ed7da
jbandura <j.bandura@borderstudio.eu>
2015-12-11 10:23:47 +0100

    add interpolation for error messages

--- addon/utils/message-provider.js
+++ addon/utils/message-provider.js
@@ -8,14 +8,42 @@ export default Ember.Object.extend({
     "too short": "too short"
   },
 
+  /**
+   * gets a validation message based on string returned
+   *
+   * @param {string|Object} if message is a string it is looked up in
+   *  defaultMessages/i18n. If it's an object it should contain keys message
+   *  and replacements. All occurences of replacements in message will be then
+   *  replaced according to the replacements hash.
+   */
   getMessage(validation) {
-    if(Ember.i18n && Ember.i18n.t(validation)) {
-      return Ember.i18n.t(validation);
+    if (typeof validation === 'string' || validation instanceof String) {
+      return this._fetchMessage(validation, {});
+    }
+
+    return this._fetchMessage(validation.message, validation.replacements);
+  },
+
+  _fetchMessage(msg, replacements) {
+    if(Ember.i18n) {
+      return Ember.i18n.t(`validation.${msg}`, replacements);
     }
     let i18nService = this.get('container') ? this.get('container').lookup('service:i18n') : null;
-    if (i18nService && i18nService.t(validation)) {
-      return i18nService.t(validation);
+    if (i18nService) {
+      return i18nService.t(`validation.${msg}`, replacements);
     }
-    return this.get(`defaultMessages.${validation}`) || validation;
+    return this._interpolateMessage(msg, replacements);
+  },
+
+  _interpolateMessage(msg, replacements) {
+    let message = this.get(`defaultMessages.${msg}`);
+    if (!message) {
+      return msg;
+    }
+    Object.keys(replacements).forEach((key) => {
+      message = message.replace(key, replacements[key]);
+    });
+
+    return message;
   }
 });
