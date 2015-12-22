2829b0356c2757197fa1949340f04bb2fcca4f65
jbandura <j.bandura@borderstudio.eu>
2015-12-22 23:17:31 +0100

    make errors disappear when textarea field set to null

--- addon/components/lf-textarea.js
+++ addon/components/lf-textarea.js
@@ -1,6 +1,18 @@
+import Ember from 'ember';
 import layout from '../templates/components/lf-textarea';
 import LFInput from './lf-input';
 
+const { observer, isNone } = Ember;
+
 export default LFInput.extend({
-  layout
+  layout,
+
+  propertyDidChange: observer('property', function() {
+    if (isNone(this.get('property'))) {
+      this.setProperties({
+        '_edited': false,
+        errorMessages: []
+      });
+    }
+  })
 });
