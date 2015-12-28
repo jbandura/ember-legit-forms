58dc1bbd277a05a70da157a6e32398ff7b0ffbe9
jbandura <j.bandura@borderstudio.eu>
2015-12-28 14:03:48 +0100

    make changes to data object visible in formValidator

--- addon/components/lf-form.js
+++ addon/components/lf-form.js
@@ -8,7 +8,7 @@ export default Component.extend({
   layout,
   formValidator: formValidator.create(),
   rules: null, //passed in
-  data: null, //passed in
+  data: computed.alias('formValidator.data'),
   fields: computed.alias('formValidator.fields'),
   _formValid: computed.alias('formValidator.isFormValid'),
 
@@ -16,7 +16,6 @@ export default Component.extend({
     this._super(...arguments);
     this.get('formValidator').setProperties({
       rules: this.get('rules'),
-      data: this.get('data'),
       container: this.get('container')
     });
   },
