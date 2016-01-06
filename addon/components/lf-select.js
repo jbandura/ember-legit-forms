02a905496907206a9c8c2fb6b70a43492370510a
jbandura <j.bandura@borderstudio.eu>
2016-01-06 23:08:21 +0100

    make lf-select clear validation state after property has been set to null

--- addon/components/lf-select.js
+++ addon/components/lf-select.js
@@ -2,13 +2,22 @@ import Ember from 'ember';
 import layout from '../templates/components/lf-select';
 import LFInputMixin from '../mixins/lf-input-mixin';
 
-const { Component } = Ember;
+const { Component, observer, isNone } = Ember;
 
 export default Component.extend(LFInputMixin, {
   layout,
   content: null, //passed in
+  prompt: '-- select --', //passed in
   valuePath: null,
   labelPath: null,
+
+  propertyDidChange: observer('property', function() {
+    if(isNone(this.get('property'))) {
+      // we have to reset fields
+      this.clearValidations();
+      return;
+    }
+  }),
 
   actions: {
     optionSelected(value) {
