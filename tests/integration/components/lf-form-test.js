36ca638b12b01cd47b9cd43e9ac0340496903972
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-09-10 18:49:34 +0200

    Add tests

--- tests/integration/components/lf-form-test.js
+++ tests/integration/components/lf-form-test.js
@@ -23,3 +23,50 @@ test('it calls the on-submit action', function(assert){
   this.$('.js-submit').trigger('click');
 });
 
+test('it displays errors on submit when preventSubmit=true', function(assert){
+  assert.expect(3);
+
+  this.set('rules', {
+    input: 'required',
+    textarea: 'required',
+    select: 'required',
+  });
+
+  this.set('onSubmit', () => {
+    assert.ok(false, 'it shouldn\'t be called at all');
+  });
+
+  this.set('selectOptions', [
+    { id: 'val1', name: 'Value 1' },
+    { id: 'val2', name: 'Value 2' },
+  ]);
+
+  this.render(hbs`
+  {{#lf-form rules=rules onSubmit=(action onSubmit) preventSubmit=true as |v|}}
+    {{lf-input
+      class='js-input'
+      name='input'
+      validate=v
+    }}
+    {{lf-textarea
+      class='js-textarea'
+      name='textarea'
+      validate=v
+    }}
+    {{lf-select
+      content=selectOptions
+      name='select'
+      class='js-select'
+      valuePath='id'
+      labelPath='name'
+      validate=v
+    }}
+    <button class="js-submit">Submit</button>
+  {{/lf-form}}`);
+
+  this.$('.js-submit').trigger('click');
+
+  assert.ok(this.$('.js-input').hasClass('has-error'), 'it should display error on submit');
+  assert.ok(this.$('.js-textarea').hasClass('has-error'), 'it should display error on submit');
+  assert.ok(this.$('.js-select').hasClass('has-error'), 'it should display error on submit');
+});
