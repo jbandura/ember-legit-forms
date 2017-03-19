e943c0f011aaa74505f6d085991f21b5696ac06b
Adrian Zalewski <aazalewski@hotmail.com>
2017-03-19 17:37:45 +0100

    E-mail validator: using native browser-based validation when available.

--- addon/validators/email-validator.js
+++ addon/validators/email-validator.js
@@ -6,9 +6,17 @@ export default Ember.Object.extend({
   validate(value) {
     if (isBlank(value)) { return; }
 
-    let emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    const input = document.createElement('input');
+    input.type = 'email';
+    input.value = value;
 
-    if (!emailRegex.test(value)) {
+    const emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+
+    const isValid = typeof input.checkValidity === 'function' ?
+      input.checkValidity() :
+      emailRegex.test(value);
+
+    if (!isValid) {
       return 'mustBeValidEmail';
     }
   }
